337285c24e5628a9eb3ecf341c296c86ed965341
Александр Ханов <fulmenius@yandex.ru>
2024-10-12 15:04:59 +0300

    search

--- tgbot.js
+++ tgbot.js
@@ -6,21 +6,69 @@ const { getSuggestions } = require("./search.js");
 const { getGuaranteedLineups } = require("./main.js");
 const { hrefOfVehicle } = require("./frontend.js");
 
+// Copypasted imports
+const VEHICLESREFRESHTIME = 1 * 60 * 60 * 1000;
+let vehicles = null;
+let whenBL = [ "", "", "", "", "", "" ];
+let whenTL = [ "", "", "", "", "" ];
+
+const nationsRu =
+    {
+        usa: "США",
+        germany: "Германия",
+        ussr: "СССР",
+        britain: "Великобритания",
+        france: "Франция",
+        italy: "Италия",
+        japan: "Япония",
+        china: "Китай",
+        sweden: "Швеция",
+        israel: "Израиль"
+    };
+
+async function fetchVehicles()
+{
+    // Fetching the database
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzq-ElAFHCDzk6dVqomddksLWLcNHbvBi2K5_4JZeh8OrejAt-isCrhleXiJYQA3A4Vnw/exec");
+    const responseText = await response.text();
+    vehicles = JSON.parse(responseText);
+}
+
+function refreshWhenLineups(lineups)
+{
+    whenBL = lineups.whenBL;
+    whenTL = lineups.whenTL;
+    whenEC = lineups.whenEC;
+}
+
+// TG bot stuff
+
 let token, refreshTime;
+
+async function loadVehicles()
+{
+    await fetchVehicles();
+    console.log("Vehicles loaded successfully");
+}
 
 try
 {
     // dev
     const configFile = require("./config.json");
-    token = configFile.tgtoken;
+    token = configFile.tgtokendev;
     refreshTime = 10;
 
     console.log("DEV LAUNCHED");
+
+    loadVehicles();
 }
 catch (e)
 {
     token = process.env.TOKEN;
     refreshTime = 30;
+
+    loadVehicles();
+    setInterval(async () => { await fetchVehicles(); }, VEHICLESREFRESHTIME);
 }
 
 const bot = new TelegramBot(token,
@@ -41,7 +89,7 @@ async function deployment()
     console.log(widgetMsg);
 };
 
-// Work
+// Channel work
 
 if (1)
 {
@@ -74,36 +122,25 @@ if (1)
     setInterval(refreshMessages, refreshTime * 1000);
 }
 
-/*bot.on('text', async msg =>
-{
-    console.log(msg);
-});*/
-
 function lineupFunction()
 {
-    const en = false;
-
     const lineups = getLineups();
 
-    //refreshWhenLineups(lineups);
+    refreshWhenLineups(lineups);
 
     // String localization
-    const gsb = en ? "*Ground SB* now" : "*Совместные СБ сейчас:*";
-    const asb = en ? "**Enduring Confrontation** now" : "*Противостояние сейчас:*";
-    const availableIn = en ? "In " : "Через ";
-    const days = en ? " d " : " д ";
-    const hours = en ? " h " : " ч ";
-    const minutes = en ? " min" : " мин";
-    const squadron = en ? "**Squadron activity** reset in" : "*Сброс полковой активности*";
-    const firstDay = en ? " (1st day of the cycle)" : " (день 1/3)";
-    const secondDay = en ? " (2nd day of the cycle)" : " (день 2/3)";
-    const thirdDay = en ? " (3rd day of the cycle)" : " (день 3/3)";
+    const gsb = "*Совместные СБ сейчас:*";
+    const asb = "*Противостояние сейчас:*";
+    const availableIn = "Через ";
+    const days = " д ";
+    const hours = " ч ";
+    const minutes = " мин";
+    const squadron = "*Сброс полковой активности*";
+    const squadronDays = [" (день 1/3)", " (день 2/3)", " (день 3/3)" ];
 
-    const weekDaysEn = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
-    const weekDaysRu = [ "вс", "пн", "вт", "ср", "чт", "пт", "сб" ];
-    const weekDays = en ? weekDaysEn : weekDaysRu;
+    const weekDays = [ "вс", "пн", "вт", "ср", "чт", "пт", "сб" ];
 
-    const searchLink = "Поиск техники (в разработке)";
+    const searchLink = "[Поиск техники](https://t.me/wtlineup_bot)";
 
     const boosty = "[Boosty](https://boosty.to/solawk)";
     const github = "[GitHub](https://github.com/solawk/wtlineup)";
@@ -146,39 +183,7 @@ function lineupFunction()
 
     // Squadron
     let squadronResetString = lineups.sqD + days + lineups.sqH + hours + lineups.sqM + minutes;
-    let cycleDay;
-    switch (lineups.sqD)
-    {
-        case 2: cycleDay = firstDay; break;
-        case 1: cycleDay = secondDay; break;
-        case 0: cycleDay = thirdDay; break;
-    }
-
-    /*const msg = new EmbedBuilder()
-        .setTitle(name)
-        .setDescription(linkDisclaimer)
-        .setURL('https://solawk.github.io/wtlineup')
-        .setThumbnail(thumbnailUrl)
-        .addFields(
-            { name: gsb,
-                value: "[**" + lineups.bottomNow + "**](" + link(lineups.bottomNow) + ") и " + "[**" + lineups.topNow + "**](" + link(lineups.topNow) + ")",
-                inline: true },
-            { name: availableIn + lineups.nextHours + hours + lineups.nextMinutes + minutes,
-                value: "[" + lineups.bottomNext + "](" + link(lineups.bottomNext) + ") и " + "[" + lineups.topNext + "](" + link(lineups.topNext) + ")",
-                inline: true },
-            { name: future,
-                value: futureLineupsString },
-            { name: asb,
-                value: aviaNowString,
-                inline: true },
-            { name: availableIn + lineups.aviaNextDays + days + lineups.aviaNextHours + hours + lineups.aviaNextMinutes + minutes,
-                value: aviaNextString,
-                inline: true },
-            { name: squadron,
-                value: squadronResetString + cycleDay },
-            { name: authors,
-                value: boosty + ", " + github }
-        );*/
+    const cycleDay = squadronDays[2 - lineups.sqD];
 
     let msg1 = "";
     let msg2 = "";
@@ -194,7 +199,113 @@ function lineupFunction()
     msg1 += "\n\n" + boosty + ", " + github + ", " + wtlineupWebsite;
 
     msg2 += "[" + lineups.bottomNow + "](" + link(lineups.bottomNow) + ") и " + "[" + lineups.topNow + "](" + link(lineups.topNow) + ")";
-    msg2 += "\n=> " + "[" + lineups.bottomNext + "](" + link(lineups.bottomNext) + ") и " + "[" + lineups.topNext + "](" + link(lineups.topNext) + ")";
+    msg2 += " => ";
+    msg2 += "[" + lineups.bottomNext + "](" + link(lineups.bottomNext) + ") и " + "[" + lineups.topNext + "](" + link(lineups.topNext) + ")";
 
     return [ msg1, msg2 ];
 }
+
+// Search work
+
+bot.onText(/\/start/, msg =>
+{
+    bot.sendMessage(msg.chat.id, "Готов искать сетапы техники! Введите её название целиком, либо введите часть и я выведу наиболее подходящие варианты");
+});
+
+bot.on("text", msg =>
+{
+    if (msg.text === "/start") return;
+
+    if (vehicles == null)
+    {
+        bot.sendMessage(msg.chat.id, "Бот только что запустился и не успел загрузить данные о технике, попробуйте снова через несколько секунд");
+        return;
+    }
+
+    const replymsg = searchFunction(msg.text.trim());
+    bot.sendMessage(msg.chat.id, replymsg, { parse_mode: "Markdown", link_preview_options: JSON.stringify({ is_disabled: true }) });
+});
+
+function searchFunction(query)
+{
+    const suggestions = getSuggestions(query, vehicles, getGuaranteedLineups, 10, AB);
+
+    // String localization
+    const name = "Результаты поиска - ";
+    const noneFound = "Подходящей техники не найдено";
+
+    const boosty = "[Boosty](https://boosty.to/solawk)";
+    const github = "[GitHub](https://github.com/solawk/wtlineup)";
+    const wtlineupWebsite = "[сайт WTLineup](https://bit.ly/wt-sim)";
+
+    // Links
+    function link(lineup)
+    {
+        return "https://solawk.github.io/wtlineup/?select=" + lineup;
+    }
+
+    let msg = name + "*" + query + "*";
+
+    for (const s of suggestions)
+    {
+        let lineupsString = "";
+
+        for (let i = 0; i < s.l.length; i++)
+        {
+            lineupsString += "[" + s.l[i] + "](" + link(s.l[i]) + ")";
+
+            const whenRu =
+                {
+                    whenNow: "доступен сейчас!",
+                    whenToday: "будет позже сегодня",
+                    whenTomorrow: "будет завтра",
+                    whenAfterTomorrow: "будет послезавтра",
+                    whenAfterDaysBefore: "будет через ",
+                    whenAfterDaysAfter: " д",
+                };
+
+            // Day
+            const isEC = s.l[i].includes("-");
+            const isBottom = s.l[i].endsWith("1");
+            const indexOfLineup = isBottom ? BL.indexOf(s.l[i]) : TL.indexOf(s.l[i]);
+            const when = !isEC ? (isBottom ? whenBL[indexOfLineup] : whenTL[indexOfLineup]) : whenIsLineup(s.l[i]);
+
+            if (when !== "")
+            {
+                let whenString = " ("
+                switch (when)
+                {
+                    case "now": whenString += whenRu.whenNow; break;
+                    case "today": whenString += whenRu.whenToday; break;
+                    case 1: whenString += whenRu.whenTomorrow; break;
+                    case 2: whenString += whenRu.whenAfterTomorrow; break;
+
+                    default:
+                        whenString += (whenRu.whenAfterDaysBefore) + (when - 1).toString() + (whenRu.whenAfterDaysAfter);
+                        break;
+                }
+                whenString += ")";
+                lineupsString += whenString;
+            }
+
+            if (i < s.l.length - 1) lineupsString += "\n";
+        }
+
+        const vname = (s.v.ruName !== "" ? s.v.ruName : s.v.enName);
+        lineupsString += "\n[Найти " + vname + " в Google](" + hrefOfVehicle(s.v).replaceAll("(", "").replaceAll(")", "") + ")";
+
+        msg += "\n\n*" + vname + "* - " + nationsRu[s.v.nation] + ", " + s.v.br.toString() + "\n";
+        msg += "\n" + lineupsString.length > 0 ? lineupsString : "-";
+    }
+
+    if (suggestions.length === 0)
+    {
+        msg += "\n" + noneFound;
+    }
+
+    msg += "\n\n" + boosty + ", " + github + ", " + wtlineupWebsite;
+
+    return msg;
+}
+
+bot.on("polling_error", console.log);
