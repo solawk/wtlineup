cec616aa2f492de39c71f2f02a362a09a24db88b
Александр Ханов <fulmenius@yandex.ru>
2024-12-22 18:06:26 +0300

    5_1 france

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ const thumbnails =
             "https://encyclopedia.warthunder.com/i/images/bf-109f-4.png",
         ],
         l5_1: [
-            "https://static.encyclopedia.warthunder.com/images/germ_pzkpfw_vi_ausf_b_tiger_iip.png",
+            "https://encyclopedia.warthunder.com/i/images/fr_amx_m4.png",
             "https://encyclopedia.warthunder.com/i/images/germ_pzkpfw_vi_ausf_b_tiger_iip.png",
             "https://encyclopedia.warthunder.com/i/images/a-26b_10.png",
             "https://encyclopedia.warthunder.com/i/images/bf-109g-14.png",
